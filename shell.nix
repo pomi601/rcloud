580aba41ec3697da83c0d25896744de72501fb8e
pomi601 <pomi601@aucklanduni.ac.nz>
2024-07-23 20:27:33 +1200

    nix: use <nixpkgs> instead of pinned tarball
    It could be more cache friendly to let the developer select their own
    <nixpkgs> using the nix-channel mechanism.

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import <nixpkgs> { config = {}; overlays = []; };
 in
-
 pkgs.mkShell {
   packages = with pkgs; [
     autoconf

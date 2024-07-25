cd1274ee8a52cf0478ea4b4bc76cefdd59bd6fda
pomi601 <pomi601@aucklanduni.ac.nz>
2024-07-25 17:49:41 +1200

    wip: added all dependencies to build.zig.zon with checksums
    and permanent download links on github or rforge (versus CRAN which
    does not provide permanent links to current versions)

--- build.zig
+++ build.zig
@@ -1,47 +1,63 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
-    // var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer _ = arena_state.deinit();
-    // const arena = arena_state.allocator();
-
-    // const target = b.standardTargetOptions(.{});
-    // const optimize = b.standardOptimizeOption(.{});
-
-    const wf = b.addWriteFiles();
-
-    std.debug.print("b.exe_dir = {?s}\n", .{b.exe_dir});
-    std.debug.print("b.lib_dir = {?s}\n", .{b.lib_dir});
-    std.debug.print("b.dest_dir = {?s}\n", .{b.dest_dir});
-
-    for (lib_tarballs) |tarball| {
-        const r_build = b.addSystemCommand(&.{"scripts/r-cmd-install.sh"});
-        r_build.addArgs(&.{
-            "--no-docs",
-            "--no-multiarch",
-            "--no-staged-install",
-            "-l",
-        });
-        r_build.addDirectoryArg(wf.getDirectory());
-        r_build.addFileArg(b.path(tarball));
-
-        // figure out the path to the installed library
-        const base = std.fs.path.basename(tarball);
-        // split on _, which separates library name from its version
-        var iter = std.mem.split(u8, base, "_");
-        const lib_name = iter.next() orelse unreachable;
-
-        const install_sys = b.addInstallDirectory(.{
-            .source_dir = wf.getDirectory().path(b, lib_name),
-            .install_dir = .lib,
-            .install_subdir = lib_name,
-        });
-        install_sys.step.dependOn(&r_build.step);
-
-        b.getInstallStep().dependOn(&install_sys.step);
-    }
+fn r_build_step(
+    b: *std.Build,
+    libdir: *std.Build.Step.WriteFile,
+    src: *std.Build.Dependency,
+) *std.Build.Step.Run {
+    const r_build = b.addSystemCommand(&.{"R"});
+    r_build.addArgs(&.{
+        "CMD",
+        "INSTALL",
+        "--no-docs",
+        "--no-multiarch",
+        "--no-staged-install",
+        "-l",
+    });
+    r_build.addDirectoryArg(libdir.getDirectory());
+    r_build.addDirectoryArg(src.path(""));
+    r_build.setEnvironmentVariable("CC", "zig cc");
+    r_build.setEnvironmentVariable("CXX", "zig c++");
+    return r_build;
 }
 
-const lib_tarballs = [_][]const u8{
-    "build/out/dist/cran/sys_3.4.2.tar.gz",
-};
+fn r_install_step(
+    b: *std.Build,
+    dep: *std.Build.Dependency,
+    libdir: *std.Build.Step.WriteFile,
+    name: []const u8,
+) *std.Build.Step.InstallDir {
+    const r_build = r_build_step(b, libdir, dep);
+    const r_install = b.addInstallDirectory(.{
+        .source_dir = libdir.getDirectory().path(b, name),
+        .install_dir = .lib,
+        .install_subdir = name,
+    });
+    r_install.step.dependOn(&r_build.step);
+    return r_install;
+}
+
+pub fn build(b: *std.Build) !void {
+
+    // the directory to which R libraries are installed during the
+    // build step
+    const r_libdir = b.addWriteFiles();
+
+    const r_sys = r_install_step(
+        b,
+        b.dependency("r_sys", .{}),
+        r_libdir,
+        "sys",
+    );
+
+    const r_askpass = r_install_step(
+        b,
+        b.dependency("r_askpass", .{}),
+        r_libdir,
+        "askpass",
+    );
+    r_askpass.step.dependOn(&r_sys.step);
+
+    b.getInstallStep().dependOn(&r_askpass.step);
+    b.getInstallStep().dependOn(&r_sys.step);
+}

a42bee1a12722e94540af271422685af44c4661b
pomi601 <pomi601@aucklanduni.ac.nz>
2024-09-18 17:01:08 +1200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "RCloud development with Zig build";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    zig.url = "github:mitchellh/zig-overlay";
+    zig.inputs.nixpkgs.follows = "nixpkgs";
+
+    zls.url = "github:zigtools/zls";
+    zls.inputs.nixpkgs.follows = "nixpkgs";
+    zls.inputs.zig-overlay.follows = "zig";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { flake-utils, nixpkgs, zig, zls, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # this overrides zls to skip checks, because their things sometimes fail
+        zlsOverlay = final: prev: {
+          zlsOverride = zls.packages.${system}.zls.overrideAttrs { doCheck = false; };
+        };
+
+        overlays = [
+          zig.overlays.default
+          zlsOverlay
+        ];
+
+        pkgs = import nixpkgs {
+          overlays = overlays;
+          system = system;
+        };
+
+      in
+        {
+          overlays = overlays;
+
+          devShells.default =
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                autoconf
+                automake
+                cairo
+                curl
+                killall
+                git
+                gnumake
+                icu
+                libxcrypt
+                libxml2
+                nodejs-slim
+                nodePackages.npm
+                openssl
+                pkg-config
+                redis
+                R
+                rPackages.codetools
+                rPackages.Matrix
+
+                zig.packages.${system}.master
+                zlsOverride
+              ];
+
+              LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+            };
+        });
+}
